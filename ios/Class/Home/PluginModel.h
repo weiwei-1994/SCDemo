58a80dfd55f61b6bca590f1f5240f8b104b9d92a
shenWenFeng <wenfeng.shen@wyze.cn>
2022-07-06 11:07:22 +0800

    ios 工程可以配置多插件

---
+++ ios/Class/Home/PluginModel.h
@@ -0,0 +1,24 @@
+//
+//  PluginModel.h
+//  SCDemo
+//
+//  Created by shenWenFeng on 2022/7/5.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PluginModel : NSObject
+
+@property(nonatomic,strong)NSString * moduleName;
+
+@property(nonatomic,strong)NSString * filePath;
+
+
+
+@end
+
+
+
+NS_ASSUME_NONNULL_END
