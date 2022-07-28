259fe76123c514745a16df6f518b12675a955c39
yangjie <yangjie@ihealthlabs.com.cn>
2022-07-28 14:25:09 +0800

    删除exception-handler

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-//import Navigate from './Plugin/Navigate/NavigateComponent'
-import Push from './Plugin/Push'
+import Navigate from './Plugin/Navigate/NavigateComponent'
+//import Push from './Plugin/Push'
+import App from './Plugin/App'
 
-AppRegistry.registerComponent(appName, () => Push);
+AppRegistry.registerComponent(appName, () => App);
