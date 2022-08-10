fa5651d2f193ff28c2e8261abfacdc30d6a6fceb
yangjie <yangjie@ihealthlabs.com.cn>
2022-08-10 17:59:49 +0800

    本地打包提交

--- Plugin/App.tsx
+++ Plugin/App.tsx
@@ -6,76 +6,77 @@
  * @flow strict-local
  */
 
-import React, { useState, useEffect } from 'react';
-import {
-  StyleSheet,
-  Platform,
-  View,
-  Text,
-  Button,
-  NativeModules
-} from 'react-native';
-import { NavtiveView,registExceptionHandler} from 'react-native-ww-plugin-sdk'
-import {name} from '../app.json'
-
-const App = (props: any) => {
-  const [count, setCount] = useState(1);
-
-  const callAndroidMoudle = (message: string) => {
-    if (Platform.OS == 'android') {
-      setCount(count + 1);
-      const androidNativeMoudle = NativeModules.ToastExample;
-      androidNativeMoudle.show(message, 0);
-    }
-  }
-
-
-  const androidCompent = () => {
-    if (Platform.OS == 'android') {
-      return (
-        <View style={styles.androidArea}>
-          <Button
-            title='调用安卓原生模块'
-            onPress={() => callAndroidMoudle('调用安卓原生模块')} />
-          <NavtiveView
-            style={styles.nativeView}
-            titleText={count.toString()}
-            onChangeMessage={(nativeEvent: any) => callAndroidMoudle(nativeEvent.message)}
-          />
-        </View>
-      )
-    }
-    return null;
-  }
-
-  useEffect(() => {
-    registExceptionHandler(name);
-  })
-
-  return (
-    <View style={styles.contanin}>
-      <Button
-        title="进入设置页"
-        onPress={() => props.navigation.navigate("Setting1")} />
-      {androidCompent()}
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  contanin: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  androidArea: {
-    marginTop: 30
-  },
-  nativeView: {
-    width: 100,
-    height: 100,
-    marginTop: 50
-  }
-});
-
-export default App;
+ import React, { useState, useEffect } from 'react';
+ import {
+   StyleSheet,
+   Platform,
+   View,
+   Text,
+   Button,
+   NativeModules
+ } from 'react-native';
+ import { NavtiveView,registExceptionHandler} from 'react-native-ww-plugin-sdk'
+ import {name} from '../app.json'
+ 
+ const App = (props: any) => {
+   const [count, setCount] = useState(1);
+ 
+   const callAndroidMoudle = (message: string) => {
+     if (Platform.OS == 'android') {
+       setCount(count + 1);
+       const androidNativeMoudle = NativeModules.ToastExample;
+       androidNativeMoudle.show(message, 0);
+     }
+   }
+ 
+ 
+   const androidCompent = () => {
+     if (Platform.OS == 'android') {
+       return (
+         <View style={styles.androidArea}>
+           <Button
+             title='调用安卓原生模块'
+             onPress={() => callAndroidMoudle('调用安卓原生模块')} />
+           <NavtiveView
+             style={styles.nativeView}
+             titleText={count.toString()}
+             onChangeMessage={(nativeEvent: any) => callAndroidMoudle(nativeEvent.message)}
+           />
+         </View>
+       )
+     }
+     return null;
+   }
+ 
+   useEffect(() => {
+     registExceptionHandler(name);
+   })
+ 
+   return (
+     <View style={styles.contanin}>
+       <Button
+         title="进入设置页"
+         onPress={() => props.navigation.navigate("Setting")} />
+       {androidCompent()}
+     </View>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   contanin: {
+     flex: 1,
+     justifyContent: 'center',
+     alignItems: 'center'
+   },
+   androidArea: {
+     marginTop: 30
+   },
+   nativeView: {
+     width: 100,
+     height: 100,
+     marginTop: 50
+   }
+ });
+ 
+ export default App;
+ 
